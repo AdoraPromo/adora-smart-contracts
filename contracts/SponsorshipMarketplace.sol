09609fb7c4deba226d02ef611474bb6fbf09ebd5
Kristians Kuhta <kristians-kuhta@users.noreply.github.com>
2023-10-16 14:12:52 +0300

    Refactor marketplace contract to reflect latest architecture

--- contracts/SponsorshipMarketplace.sol
+++ contracts/SponsorshipMarketplace.sol
@@ -10,92 +10,74 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "@tableland/evm/contracts/utils/TablelandDeployments.sol";
 import "@tableland/evm/contracts/utils/SQLHelpers.sol";
 
+import {ApeCoin} from "./ApeCoin.sol";
+
 contract SponsorshipMarketplace is ERC721Holder {
   // is FunctionsClient, ConfirmedOwner {
 
-  struct User {
-    string id;
-    address accountAddress;
+  enum Status {
+    NEW,
+    ACCEPTED,
+    REDEEMED,
+    WITHDRAWN
   }
 
-  struct Proposal {
-    string id;
-    string creatorId;
-    string sponsorId;
-    uint256 maxPaymentAmount;
-    uint256 paymentPerLike;
-    string requirements;
-    uint256 deliveryDeadline;
+  struct Deal {
+    Status status;
+    address sponsor;
+    address creator;
+    bytes32 termsHash;
+    // Encrypted using Chainlink Functions publicEncryptionKey
+    string encryptedSymmetricKey;
+    // Encrypted using symmetricKey
+    string encryptedTerms;
+    uint256 redemptionExpiration;
+    uint256 maxPayment;
+    string encryptedTweetId;
   }
 
   using Strings for uint256;
 
-  // Deal status codes
-  uint256 public constant UNKNOWN = 0;
-  uint256 public constant NEW = 1;
-  uint256 public constant WITHDRAWN = 2;
-  uint256 public constant ACCEPTED = 3;
-  uint256 public constant CANCELLED = 4;
+  ApeCoin public s_paymentToken;
 
-  mapping(string dealId => uint256 status) private s_dealStatuses;
-  mapping(string dealId => uint256 timestamp) private s_dealDeliveryDeadlines;
-  mapping(string dealId => uint256 limit) private s_dealMaxPaymentAmounts;
-  mapping(string creatorId => bool exists) private s_creatorIds;
-  mapping(string sponsorId => bool exists) private s_sponsorIds;
+  uint256 public s_tableId;
+  string public s_tableName;
 
-  uint256 public constant USERS_TABLE_INDEX = 0;
-  uint256 public constant DEALS_TABLE_INDEX = 1;
-
-  address public s_paymentToken;
-
-  uint256[2] public s_tableIds;
-  string[2] public s_tableNames;
+  mapping(bytes32 id => Deal deal) private s_deals;
 
   error PaymentTokenMissing();
-  error DealIdMissing();
-  error DeliveryDeadlineMustBeInFuture();
-  error RequirementsMissing();
+  error MaxValueAllowanceMissing();
   error DealAlreadyExists();
+  error RedemptionExpirationMustBeInFuture();
+  error TermsHashMissing();
+  error EncryptedSymmetricKeyMissing();
+  error EncryptedTermsMissing();
+  error MaxPaymentMissing();
 
-  error CreatorIdMissing();
-  error SponsorIdMissing();
-  error CreatorIdsDiffer();
-  error SponsorIdsDiffer();
-  error SponsorAddressMustMatchSender();
-  error PaymentPerLikeMissing();
-  error MaxPaymentAmountMissing();
-
-  event DealCreated(string dealId);
+  event DealCreated(bytes32 dealId);
 
   constructor(address paymentToken) {
     _requirePaymentToken(paymentToken);
 
-    s_paymentToken = paymentToken;
+    s_paymentToken = ApeCoin(paymentToken);
 
-    _createTable(
-      USERS_TABLE_INDEX,
-      "users",
-      "id text primary key, "
-      "twitter_account_id text, "
-      "address text"
-    );
-
-    _createTable(
-      DEALS_TABLE_INDEX,
+    (uint256 tableId, string memory tableName) = _createTable(
       "deals",
       "id text primary key, "
-      "creator_id text, "
-      "sponsor_id text, "
       "status text, "
-      "max_payment_amount text, "
-      "payment_per_like text, "
-      "payment_token text, "
-      "tweet_id text, "
-      "requirements text, "
-      "created_at integer, "
-      "delivery_deadline integer, "
-      "delivery_attempts integer"
+      "sponsor_address text, "
+      "creator_address text, "
+      "terms_hash text, "
+      "encrypted_symmetric_key text, "
+      "encrypted_terms text, "
+      "redemption_expiration integer, "
+      "max_payment text, "
+      "redeemed_amount text, "
+      "encrypted_tweet_id text"
     );
+
+    s_tableId = tableId;
+    s_tableName = tableName;
   }
 
   function _requirePaymentToken(address paymentToken) internal pure {
@@ -104,232 +86,160 @@ contract SponsorshipMarketplace is ERC721Holder {
     }
   }
 
-  function createDeal(User calldata creator, User calldata sponsor, Proposal calldata proposal) external {
-    _runCreateDealValidations(creator, sponsor, proposal);
+  function createDeal(
+    bytes32 termsHash,
+    string calldata encryptedSymmetricKey,
+    string calldata encryptedTerms,
+    uint256 maxPayment,
+    uint256 redemptionExpiration
+  ) external returns (bytes32 dealId) {
+    _runCreateDealValidations(termsHash, encryptedSymmetricKey, encryptedTerms, maxPayment, redemptionExpiration);
 
-    s_dealStatuses[proposal.id] = NEW;
-    s_dealDeliveryDeadlines[proposal.id] = proposal.deliveryDeadline;
-    s_dealMaxPaymentAmounts[proposal.id] = proposal.maxPaymentAmount;
+    _requireMarketplacePaymentTokenAllowance(maxPayment);
 
-    _handleCreatorAndSponsorRecords(creator, sponsor);
+    Deal memory deal = Deal(
+      Status.NEW,
+      msg.sender,
+      address(0),
+      termsHash,
+      encryptedSymmetricKey,
+      encryptedTerms,
+      redemptionExpiration,
+      maxPayment,
+      ""
+    );
 
-    _insertDealRow(proposal);
+    dealId = keccak256(abi.encode(deal));
+    _requireDealDoesNotExist(dealId);
 
-    emit DealCreated(proposal.id);
+    s_deals[dealId] = deal;
+
+    TablelandDeployments.get().mutate(address(this), s_tableId, _dealInsertSql(dealId, deal));
+
+    emit DealCreated(dealId);
   }
 
-  function _runCreateDealValidations(
-    User calldata creator,
-    User calldata sponsor,
-    Proposal calldata proposal
-  ) internal view {
-    _requireDealId(proposal.id);
-    _requireDealDoesNotExist(proposal.id);
-
-    _requireCreatorId(proposal.creatorId);
-    _requireCreatorIdMatchesDeal(creator.id, proposal.creatorId);
-
-    _requireSponsorId(proposal.sponsorId);
-    _requireSponsorIdMatchesDeal(sponsor.id, proposal.sponsorId);
-
-    _requireSponsorAddressMatchesSender(sponsor.accountAddress);
-
-    _requireDealRequirements(proposal.requirements);
-    _requireDeliveryDeadlineInFuture(proposal.deliveryDeadline);
-
-    _requirePaymentPerLike(proposal.paymentPerLike);
-    _requireMaxPaymentAmount(proposal.maxPaymentAmount);
-  }
-
-  // TODO: use proper function ordering in this file
-  function _requireCreatorIdMatchesDeal(string calldata creatorId, string calldata proposalCreatorId) internal pure {
-    if (keccak256(abi.encodePacked(creatorId)) != keccak256(abi.encodePacked(proposalCreatorId))) {
-      revert CreatorIdsDiffer();
-    }
-  }
-
-  function _requireSponsorIdMatchesDeal(string calldata sponsorId, string calldata proposalSponsorId) internal pure {
-    if (keccak256(abi.encodePacked(sponsorId)) != keccak256(abi.encodePacked(proposalSponsorId))) {
-      revert SponsorIdsDiffer();
-    }
-  }
-
-  function _requireSponsorAddressMatchesSender(address accountAddress) internal view {
-    if (accountAddress != msg.sender) {
-      revert SponsorAddressMustMatchSender();
-    }
-  }
-
-  function _createTable(uint256 index, string memory prefix, string memory columns) internal {
+  // TODO: use proper smart contract member organization in this file
+  //       (external/public, internal, internal view, internal pure functions order + other ordering)
+  function _createTable(
+    string memory prefix,
+    string memory columns
+  ) internal returns (uint256 tableId, string memory tableName) {
     string memory schema = string.concat(SQLHelpers.toCreateFromSchema(columns, prefix), ";");
     string memory chainId = Strings.toString(block.chainid);
 
-    uint256 tableId = TablelandDeployments.get().create(address(this), schema);
-    string memory fullName = string.concat(prefix, "_", chainId, "_", Strings.toString(tableId));
-
-    s_tableIds[index] = tableId;
-    s_tableNames[index] = fullName;
+    tableId = TablelandDeployments.get().create(address(this), schema);
+    tableName = string.concat(prefix, "_", chainId, "_", Strings.toString(tableId));
   }
 
-  function _handleCreatorAndSponsorRecords(User calldata creator, User calldata sponsor) internal {
-    if (!_creatorExists(creator.id)) {
-      _insertCreatorRow(creator.id, "NULL", creator.accountAddress);
-    }
+  function _runCreateDealValidations(
+    bytes32 termsHash,
+    string calldata encryptedSymmetricKey,
+    string calldata encryptedTerms,
+    uint256 maxPayment,
+    uint256 redemptionExpiration
+  ) internal view {
+    _requireTermsHash(termsHash);
 
-    if (!_sponsorExists(sponsor.id)) {
-      _insertSponsorRow(sponsor.id);
-    }
-  }
+    _requireEncryptedSymmetricKey(encryptedSymmetricKey);
 
-  function _creatorExists(string calldata creatorId) internal view returns (bool) {
-    return s_creatorIds[creatorId];
-  }
+    _requireEncryptedTerms(encryptedTerms);
 
-  function _sponsorExists(string calldata sponsorId) internal view returns (bool) {
-    return s_sponsorIds[sponsorId];
-  }
+    _requireMaxPayment(maxPayment);
 
-  function _insertCreatorRow(
-    string calldata creatorId,
-    string memory twitterAccountId,
-    address creatorAddress
-  ) internal {
-    _insertUserRow(creatorId, twitterAccountId, creatorAddress);
-  }
-
-  function _insertSponsorRow(string calldata sponsorId) internal {
-    _insertUserRow(sponsorId, "NULL", msg.sender);
-  }
-
-  function _insertUserRow(string calldata id, string memory twitter_account_id, address userAddress) internal {
-    uint256 tableId = s_tableIds[USERS_TABLE_INDEX];
-
-    TablelandDeployments.get().mutate(
-      address(this),
-      tableId,
-      SQLHelpers.toInsert(
-        "users",
-        tableId,
-        "id,twitter_account_id,address",
-        string.concat(
-          SQLHelpers.quote(id),
-          ",",
-          SQLHelpers.quote(twitter_account_id),
-          ",",
-          SQLHelpers.quote(Strings.toHexString(userAddress))
-        )
-      )
-    );
-  }
-
-  function _insertDealRow(Proposal calldata proposal) internal {
-    uint256 tableId = s_tableIds[DEALS_TABLE_INDEX];
-
-    TablelandDeployments.get().mutate(address(this), tableId, _dealInsertSql(tableId, proposal));
+    _requireRedemptionExpirationInFuture(redemptionExpiration);
   }
 
   // "deals",
   //   "id text primary key, "
-  //   "creator_id text, "
-  //   "sponsor_id text, "
   //   "status text, "
-  //   "max_payment_amount text, "
-  //   "payment_per_like text, "
-  //   "payment_token text, "
-  //   "tweet_id text, "
-  //   "requirements text, "
-  //   "created_at integer, "
-  //   "delivery_deadline integer, "
-  //   "delivery_attempts integer"
-  function _dealInsertSql(uint256 tableId, Proposal calldata proposal) internal view returns (string memory) {
+  //   "sponsor_address text, "
+  //   "creator_address text, "
+  //   "terms_hash text, "
+  //   "encrypted_symmetric_key text, "
+  //   "encrypted_terms text, "
+  //   "redemption_expiration integer, "
+  //   "max_payment text, "
+  //   "redeemed_amount text, "
+  //   "encrypted_tweet_id text"
+  function _dealInsertSql(bytes32 dealId, Deal memory deal) internal view returns (string memory) {
     string memory columns = "id,"
-    "creator_id,"
-    "sponsor_id,"
     "status,"
-    "max_payment_amount,"
-    "payment_per_like,"
-    "payment_token,"
-    "requirements,"
-    "created_at,"
-    "delivery_deadline";
+    "sponsor_address,"
+    "creator_address,"
+    "terms_hash,"
+    "encrypted_symmetric_key,"
+    "encrypted_terms,"
+    "redemption_expiration,"
+    "max_payment,"
+    "redeemed_amount";
 
-    return SQLHelpers.toInsert("deals", tableId, columns, _dealSqlValues(proposal));
+    return SQLHelpers.toInsert("deals", s_tableId, columns, _dealInsertSqlValues(dealId, deal));
   }
 
-  function _dealSqlValues(Proposal calldata proposal) internal view returns (string memory) {
-    return
-      string.concat(
-        string.concat(
-          SQLHelpers.quote(proposal.id),
-          ",",
-          SQLHelpers.quote(proposal.creatorId),
-          ",",
-          SQLHelpers.quote(proposal.sponsorId),
-          ",",
-          SQLHelpers.quote("NEW"),
-          ",",
-          proposal.maxPaymentAmount.toString()
-        ),
-        string.concat(
-          ",",
-          proposal.paymentPerLike.toString(),
-          ",",
-          SQLHelpers.quote(Strings.toHexString(uint160(s_paymentToken))),
-          ",",
-          SQLHelpers.quote(proposal.requirements),
-          ",",
-          block.timestamp.toString(),
-          ",",
-          proposal.deliveryDeadline.toString()
-        )
-      );
-  }
+  function _requireMarketplacePaymentTokenAllowance(uint256 maxPayment) internal view {
+    uint256 allowance = s_paymentToken.allowance(msg.sender, address(this));
 
-  function _requireDealId(string calldata dealId) internal pure {
-    if (bytes(dealId).length == 0) {
-      revert DealIdMissing();
+    if (allowance >= maxPayment) {
+      revert MaxValueAllowanceMissing();
     }
   }
 
-  function _requireCreatorId(string calldata creatorId) internal pure {
-    if (bytes(creatorId).length == 0) {
-      revert CreatorIdMissing();
-    }
-  }
-
-  function _requireSponsorId(string calldata sponsorId) internal pure {
-    if (bytes(sponsorId).length == 0) {
-      revert SponsorIdMissing();
-    }
-  }
-
-  function _requireDealDoesNotExist(string calldata dealId) internal view {
-    if (s_dealStatuses[dealId] != UNKNOWN) {
+  function _requireDealDoesNotExist(bytes32 dealId) internal view {
+    if (s_deals[dealId].sponsor != address(0)) {
       revert DealAlreadyExists();
     }
   }
 
-  function _requireDeliveryDeadlineInFuture(uint256 deliveryDeadline) internal view {
-    if (deliveryDeadline <= block.timestamp) {
-      revert DeliveryDeadlineMustBeInFuture();
+  function _requireRedemptionExpirationInFuture(uint256 redemptionExpiration) internal view {
+    if (redemptionExpiration <= block.timestamp) {
+      revert RedemptionExpirationMustBeInFuture();
     }
   }
 
-  function _requireMaxPaymentAmount(uint256 maxPaymentAmount) internal pure {
-    if (maxPaymentAmount == 0) {
-      revert MaxPaymentAmountMissing();
+  function _dealInsertSqlValues(bytes32 dealId, Deal memory deal) internal pure returns (string memory) {
+    return
+      string.concat(
+        SQLHelpers.quote(string(abi.encodePacked(dealId))),
+        // The status is hardcoded as new
+        ",NEW,",
+        SQLHelpers.quote(Strings.toHexString(uint160(deal.sponsor))),
+        ",",
+        SQLHelpers.quote(Strings.toHexString(uint160(deal.creator))),
+        ",",
+        SQLHelpers.quote(string(abi.encodePacked(deal.termsHash))),
+        ",",
+        SQLHelpers.quote(deal.encryptedSymmetricKey),
+        ",",
+        SQLHelpers.quote(deal.encryptedTerms),
+        ",",
+        deal.redemptionExpiration.toString(),
+        ",",
+        SQLHelpers.quote(Strings.toHexString(deal.maxPayment))
+      );
+  }
+
+  function _requireTermsHash(bytes32 termsHash) internal pure {
+    if (termsHash == bytes32(0)) {
+      revert TermsHashMissing();
     }
   }
 
-  function _requirePaymentPerLike(uint256 paymentPerLike) internal pure {
-    if (paymentPerLike == 0) {
-      revert PaymentPerLikeMissing();
+  function _requireEncryptedSymmetricKey(string calldata encryptedSymmetricKey) internal pure {
+    if (bytes(encryptedSymmetricKey).length == 0) {
+      revert EncryptedSymmetricKeyMissing();
     }
   }
 
-  function _requireDealRequirements(string calldata requirements) internal pure {
-    if (bytes(requirements).length == 0) {
-      revert RequirementsMissing();
+  function _requireEncryptedTerms(string calldata encryptedTerms) internal pure {
+    if (bytes(encryptedTerms).length == 0) {
+      revert EncryptedTermsMissing();
+    }
+  }
+
+  function _requireMaxPayment(uint256 maxPayment) internal pure {
+    if (maxPayment == 0) {
+      revert MaxPaymentMissing();
     }
   }
 }
